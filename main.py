7a7b52fcf08a91bea91fb252dfd5b2d755970916
yasmineecosta <yasmineecosta@gmail.com>
2024-12-19 00:30:55 -0300

    encapsulated code main

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from extraiTextoPDF import ler_texto_arquivos_diretorio
+from extraiTextoPDF import TextExtractor
 from dotenv import load_dotenv
 import pandas as pd
-from download_files import download_files, limpar_arquivos
-from extrai_informacao_html import extrair_data
+from download_files import FileDownloader
+from extrai_informacao_html import HTMLInformationExtractor
 from agent_refatorado import AgentResumo
 import os
-
-class ResumoPipeline:
+class MainResumo:
     def __init__(self, api_key):
         self.agent = AgentResumo(api_key)
+        self.file_downloader = FileDownloader()
 
     def make(self, link: str):
-        extrair_data(link)
-        limpar_arquivos()
+        HTMLInformationExtractor.extrair_data(link)
+        self.file_downloader.limpar_arquivos()
 
         df = pd.read_csv("Documentos do Processo.csv")
 
@@ -23,9 +23,9 @@ class ResumoPipeline:
                 print(f"Ignorado: '{link}' não é uma URL válida.")
                 documentos_nao_baixados += 1
             else:
-                download_files(link)
+                self.file_downloader.download_files(link)
 
-        textos_processos = ler_texto_arquivos_diretorio("Arquivos_Processo")
+        textos_processos = TextExtractor.ler_texto_arquivos_diretorio("Arquivos_Processo")
         resumo = self.agent.gerar_resumo(textos_processos)
 
         return resumo
@@ -33,7 +33,7 @@ class ResumoPipeline:
 if __name__ == "__main__":
     load_dotenv('.env')
     api_key = os.getenv("API_KEY")
-    pipeline = Resumo(api_key)
+    pipeline = MainResumo(api_key)
 
     user_input = str(input("Digite o link: "))
     print(pipeline.make(user_input))
