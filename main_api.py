a8c5d9725cbca5a0df3b7d038880e835949bc518
yasmineecosta <yasmineecosta@gmail.com>
2024-12-19 00:29:01 -0300

    encapsulated code MainAPI

--- main_api.py
+++ main_api.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI, Request
-from main import make
+from agent_refatorado import AgentResumo
+
+class APIResumo():
+    def __init__(self, agent):
+        self.agent = agent
+
+    async def fazer_resumo(self, request: Request):
+        json = await request.json()
+        link = json["link"]
+        resumo = self.agent.make(link)
+        retJson = {
+            "message": resumo
+        }
+
+        return retJson
+
+
 
 app = FastAPI()
 
+agent = AgentResumo()
+api_resumo = APIResumo(agent)
+
 @app.post('/fazer_resumo')
-async def fazer_resumo(request: Request):
-    json = await request.json()
-
-    link = json["link"]
-
-    resumo = make(link)
-
-    retJson = {
-        "message": resumo
-    }
-
-    return retJson
+async def fazer_resumo_endpoint(request: Request):
+    return await api_resumo.fazer_resumo(request)
 
